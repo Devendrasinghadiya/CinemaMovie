8383b20675daa1f364d07551b54e8f7d76b58bd2
devendra singhadiya <159245178+Devendrasinghadiya@users.noreply.github.com>
2025-09-08 08:58:07 +0530

    Update page.tsx

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -5,5 +5,5 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://embed.cineflow.us/api/v5/${mediaType}/${id}/${imdb_id}`} />;
+  return <EmbedPlayer url={`https://embed.cineflow.us/api/v5/tv/${id}/${imdb_id}`} />;
 }

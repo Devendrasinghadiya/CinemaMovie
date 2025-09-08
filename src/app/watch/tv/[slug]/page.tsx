007a01fd6d16bbbfba329f63377c7b0515f31ea5
devendra singhadiya <159245178+Devendrasinghadiya@users.noreply.github.com>
2025-09-08 09:00:43 +0530

    Update page.tsx

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -3,6 +3,7 @@ import EmbedPlayer from '@/components/watch/embed-player';
 
 export const revalidate = 3600;
 
+
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
   return <EmbedPlayer url={`https://embed.cineflow.us/api/v5/tv/${id}/${imdb_id}`} />;
